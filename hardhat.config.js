ac16a923a71b3181951331d0516b1ad815cddd05
Summer Burgess <94207600+sabsc98@users.noreply.github.com>
2022-02-23 20:36:42 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/g16FCEzEJMQy7kgOIBwXMNFUqtqkG1vI',
-      accounts: ['db92e0f63c4fbe6f88dea9c2b08f3f8cf0540d72a36a07c96e0632bbebcb0a76'],
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/',
+      accounts: ['db92e0f63c4fbe6f88dea9c2b08f3f8cf0540d72a36a000000000000'],
     },
   },
 };
